d98025c87f20b6474c707eccc431c43422c5e0bf
kazormia296 <88490667+kazormia296@users.noreply.github.com>
2026-07-22 04:58:23 +0900

    fix: recover Linux release snapshots for v0.9.1 (#17)
    Co-authored-by: kazormia296 <kazormia296@users.noreply.github.com>

--- tools/release/use_archlinux_snapshot.sh
+++ tools/release/use_archlinux_snapshot.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-readonly SNAPSHOT_DATE="2026/07/01"
+readonly SNAPSHOT_DATE="2026/06/01"
 cat > /etc/pacman.d/mirrorlist <<EOF
 Server = https://archive.archlinux.org/repos/${SNAPSHOT_DATE}/\$repo/os/\$arch
 EOF
